908b35a038db6d7c6c071a02212ac0843879d9c8
Riley Bittle <rbittle34@gmail.com>
2017-03-08 19:39:25 -0600

    Added empty logic for CSP and parsing inputs

--- csp.py
+++ csp.py
@@ -12,12 +12,32 @@ parser.add_argument("consistency", action="store", help="Consistency-enforcing p
 args = parser.parse_args()
 
 
+class CSPsolver:
+
+    def __init__(self, var, con, ce):
+        self.var = parse_vars(var)
+        self.con = parse_con(con)
+        self.fc = ce
+
+    def solve(self):
+        pass
+
+def parse_vars(var_file):
+    pass
+
+def parse_con(con_file):
+    pass
+
+
 
 if __name__ == "__main__":
     try:
         var = open(args.var, "r")
         con = open(args.con, "r")
+        ce  = True if args.consistency == "fc" else False
 
+        problem = CSPsolver(var, con, ce)
+        problem.solve()
 
     except Exception e:
         print(e)
