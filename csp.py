c4252ccee7f431c42ec643ebab4ad00677ec6fe5
Danielparkgit <academydanielpark@gmail.com>
2017-03-17 23:06:49 -0500

    Update csp.py
    Added check for forward checking.

--- csp.py
+++ csp.py
@@ -197,10 +197,13 @@ class CSPsolver:
         print(solution, "solution")
         
         # Failure is printed if the variables do not follow the constraints. Skipped over when forward checking is applied.
-        # if variable != constraint:
-        failure = self.backtrack_search()
-        print(failure, "failure")
-        
+        if variable != constraint:
+            if self.fc = false
+                failure = self.backtrack_search()
+                print(failure, "failure")
+            if self.fc = true
+                return None
+            
 def parse_vars(var_file):
     '''Converts the input file into a dictionary, where
     the key is the variable name and the value is an array
